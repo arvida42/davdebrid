48962d7df60075ff70040b69c3fb3f3985413690
arvida42 <160337259+arvida42@users.noreply.github.com>
2024-10-27 14:34:47 +0100

    Update to API v4.1

--- src/lib/debrid/alldebrid.js
+++ src/lib/debrid/alldebrid.js
@@ -19,33 +19,19 @@ export default class AllDebrid extends Debrid {
   }
 
   async getRecentFiles(){
-    return this.getFiles();
+    const res = await this.#request('GET', '/v4.1/magnet/status');
+    if(res.data.magnets.length === 0){
+      return [];
+    }
+    return this.#getFilesByMagnets(res.data.magnets.slice(0, 30)); 
   }
 
   async getFiles(){
-    const res = await this.#request('GET', '/magnet/status');
-    const files = [];
-    for(let magnet of res.data.magnets){
-      if(magnet.status != 'Ready'){
-        continue;
-      }
-      for(let index in magnet.links){
-        let link = magnet.links[index];
-        files.push({
-          name: link.filename,
-          size: link.size,
-          type: fileType(link.filename),
-          url: link.link,
-          id: `${magnet.id}:${index}`,
-          lastModified: new Date(magnet.completionDate * 1000),
-          parent: {
-            id: magnet.id,
-            name: magnet.filename
-          }
-        });
-      }
+    const res = await this.#request('GET', '/v4.1/magnet/status');
+    if(res.data.magnets.length === 0){
+      return [];
     }
-    return files;   
+    return this.#getFilesByMagnets(res.data.magnets); 
   }
 
   async getDownload(file){
@@ -54,12 +40,54 @@ export default class AllDebrid extends Debrid {
       throw new Error(`No url available in alldebrid file object`);
     }
     const query = {link: file.url};
-    const res = await this.#request('GET', '/link/unlock', {query});
+    const res = await this.#request('GET', '/v4/link/unlock', {query});
     return res.data.link;
   }
 
   async getUserHash(){
     return createHash('md5').update(this.#apiKey).digest('hex');
+  }
+
+  async #getFilesByMagnets(magnets){
+    const res = await this.#request('GET', '/v4/magnet/files', {query: {id: magnets.map(magnet => magnet.id)}});
+    const magnetById = magnets.reduce((acc, magnet) => {
+      acc[magnet.id] = magnet;
+      return acc;
+    }, {});
+    const files = [];
+    for(let magnet of res.data.magnets){
+      magnet = Object.assign(magnetById[magnet.id], magnet);
+      files.push(...this.#getFilesFromNode(magnet.files).map((file, index) => {
+        file.id = `${magnet.id}:${index}`,
+        file.lastModified = new Date(magnet.completionDate * 1000);
+        file.parent = {
+          id: magnet.id,
+          name: magnet.filename
+        };
+        return file;
+      }));
+    }
+    return files;
+  }
+
+  #getFilesFromNode(node){
+    const files = [];
+    for(let file of node){
+      if(file.e){
+        files.push(...this.#getFilesFromNode(file.e));
+      }else{
+        files.push({
+          name: file.n,
+          size: file.s,
+          type: fileType(file.n),
+          url: file.l,
+          id: ``,
+          lastModified: ``,
+          parent: {}
+        });
+      }
+    }
+    return files;
   }
 
   async #request(method, path, opts){
@@ -78,12 +106,20 @@ export default class AllDebrid extends Debrid {
       }, opts.query || {})
     });
 
-    const url = `https://api.alldebrid.com/v4${path}?${new URLSearchParams(opts.query).toString()}`;
+    const queryParts = [];
+    for(const [key, value] of Object.entries(opts.query)){
+      if(Array.isArray(value)){
+        value.forEach(v => queryParts.push(`${key}[]=${encodeURIComponent(v)}`));
+      }else{
+        queryParts.push(`${key}=${encodeURIComponent(value)}`);
+      }
+    }
+
+    const url = `https://api.alldebrid.com${path}?${queryParts.join('&')}`;
     const res = await fetch(url, opts);
     const data = await res.json();
 
     if(data.status != 'success'){
-      console.log(data);
       switch(data.error.code || ''){
         case 'AUTH_BAD_APIKEY':
         case 'AUTH_MISSING_APIKEY':
